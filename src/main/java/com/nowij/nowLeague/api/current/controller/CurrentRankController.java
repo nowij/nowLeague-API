c1f257a949c75b60a3496997f5e31c0cb823fcef
nowij <nowijdl@gmail.com>
2024-05-09 16:50:09 +0900

    메서드 추가

--- src/main/java/com/nowij/nowLeague/api/current/controller/CurrentRankController.java
+++ src/main/java/com/nowij/nowLeague/api/current/controller/CurrentRankController.java
@@ -1,13 +1,23 @@
 package com.nowij.nowLeague.api.current.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/current/rank")
+@RequestMapping("/api/current")
 @RestController
 public class CurrentRankController {
     // 순위 및 일정 조회
+    @GetMapping("/rank/schedule")
+    public void allRankAndSchedule() {
+
+    }
+
+    @GetMapping("/rank/schedule/{team}")
+    public void teamRankAndSchedule(@PathVariable String team) {
+
+    }
 
     // 그래프
 }
