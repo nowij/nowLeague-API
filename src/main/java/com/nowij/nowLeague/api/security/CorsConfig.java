bc3257de1f04ec598f6491483f97f1a8ca86aa9b
nowij <nowijdl@gmail.com>
2025-04-04 15:48:47 +0900

    CORS 설정 추가

---
+++ src/main/java/com/nowij/nowLeague/api/security/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.nowij.nowLeague.api.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowCredentials(true)
+                .exposedHeaders("*")
+                .maxAge(3600);
+    }
+}
