4d6848dd6dc435f85dc32c79a0ad9eb2ae6eb9f6
nowij <nowijdl@gmail.com>
2025-04-15 09:01:29 +0900

    api path 수정

--- src/main/java/com/nowij/nowLeague/api/common/controller/CommonController.java
+++ src/main/java/com/nowij/nowLeague/api/common/controller/CommonController.java
@@ -1,16 +1,16 @@
 package com.nowij.nowLeague.api.common.controller;
 
 import com.nowij.nowLeague.api.common.dto.SeasonDTO;
-import com.nowij.nowLeague.api.common.model.*;
+import com.nowij.nowLeague.api.common.model.RoundEntity;
+import com.nowij.nowLeague.api.common.model.TeamEntity;
 import com.nowij.nowLeague.api.common.service.CommonService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/common")
 public class CommonController {
     private CommonService commonService;
 
@@ -21,6 +21,11 @@ public class CommonController {
     @GetMapping("/seasons")
     public List<SeasonDTO> selectSeasons() {
         return commonService.selectAllSeasons();
+    }
+
+    @GetMapping("/today/season")
+    public String selectTodaySeason(@RequestParam String month) {
+        return commonService.selectTodaySeason(month);
     }
 
     @GetMapping("/rounds")
--- src/main/java/com/nowij/nowLeague/api/result/controller/GameResultController.java
+++ src/main/java/com/nowij/nowLeague/api/result/controller/GameResultController.java
@@ -1,14 +1,17 @@
-package com.nowij.nowLeague.api.gameResult.controller;
+package com.nowij.nowLeague.api.result.controller;
 
-import com.nowij.nowLeague.api.gameResult.model.GameResultEntity;
-import com.nowij.nowLeague.api.gameResult.service.GameResultService;
+import com.nowij.nowLeague.api.result.model.GameRankingDTO;
+import com.nowij.nowLeague.api.result.model.GameResultEntity;
+import com.nowij.nowLeague.api.result.service.GameResultService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/game")
+import java.util.List;
+
+@RequestMapping("/game")
 @RestController
 public class GameResultController {
     private GameResultService gameResultService;
